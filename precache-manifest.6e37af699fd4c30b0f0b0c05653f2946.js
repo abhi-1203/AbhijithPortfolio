35518cc0044a8c9b39433db38e7ed924a078b8a9
Abhijith <abhijith1203@gmail.com>
2020-09-26 16:38:15 +0530

    Updates

---
+++ precache-manifest.6e37af699fd4c30b0f0b0c05653f2946.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b330a6b340f7633c93cc2e70dfb64136",
+    "url": "/AbhijithPortfolio/index.html"
+  },
+  {
+    "revision": "697342a5df63ba98a2ff",
+    "url": "/AbhijithPortfolio/static/css/main.4debb400.chunk.css"
+  },
+  {
+    "revision": "5b4160b79644065422a7",
+    "url": "/AbhijithPortfolio/static/js/2.2a9e461e.chunk.js"
+  },
+  {
+    "revision": "b688c54f9855505f47aa85b5f9c54246",
+    "url": "/AbhijithPortfolio/static/js/2.2a9e461e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "697342a5df63ba98a2ff",
+    "url": "/AbhijithPortfolio/static/js/main.05ec2846.chunk.js"
+  },
+  {
+    "revision": "8db6d57eabfd08d066cc",
+    "url": "/AbhijithPortfolio/static/js/runtime-main.cd2775bf.js"
+  },
+  {
+    "revision": "9f8ebab8ac904c2d108986d96f8ab43d",
+    "url": "/AbhijithPortfolio/static/media/abhijith.9f8ebab8.JPG"
+  },
+  {
+    "revision": "af461aa7181073ffa9fae379c24b1e9d",
+    "url": "/AbhijithPortfolio/static/media/map.af461aa7.png"
+  },
+  {
+    "revision": "c1e11aca0f84f14344033887626b5854",
+    "url": "/AbhijithPortfolio/static/media/project1.c1e11aca.jpg"
+  }
+]);
